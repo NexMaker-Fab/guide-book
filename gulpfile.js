9150ba8a496b68f0550468af5c4c6aab4bc3765a
Bala Wang <xiaodan.w@outlook.com>
2022-09-28 16:41:40 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('publish', () => {
   console.log('Publish Gitbook (_book) to Github Pages');
   return gulp.src('./_book/**/*')
     .pipe($.ghPages({
-      origin: 'github',
+      origin: 'origin',
       branch: 'master'
     }));
 });
